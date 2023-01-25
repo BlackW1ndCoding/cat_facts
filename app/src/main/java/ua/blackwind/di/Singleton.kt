1957eb8981825781f45d264d26d95a8d139b140b
Andriy Tarasenko <black.wind.coding@gmail.com>
2023-01-25 22:43:25 +0200

    basic di setup

---
+++ app/src/main/java/ua/blackwind/di/Singleton.kt
@@ -0,0 +1,10 @@
+package ua.blackwind.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Singleton {
+}
